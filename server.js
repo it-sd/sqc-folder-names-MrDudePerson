19674bde75dab1c648021bec0ca7148fbacd6dcf
Joseph Green <dudeiera@gmail.com>
2023-03-05 19:24:25 -0600

    Created code to generate folder name properly

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const newFolderName = function (folders) {
+  let highestNumber = 1
 
+  for (const folderName of folders) {
+    if (folderName.startsWith('New Folder')) {
+      const folderNumberA = folderName.indexOf('(')
+      const folderNumberB = folderName.indexOf(')')
+
+      let number = 1
+
+      if (folderNumberA !== -1 && folderNumberB !== -1) {
+        number = parseInt(folderName.substring(folderNumberA + 1, folderNumberB), 10)
+      }
+
+      highestNumber = Math.max(highestNumber, number)
+    }
+  }
+
+  return `New Folder ${highestNumber + 1}`
 }
+
+// Test case:
+const folders = ['New Folder', 'New Folder (2)', 'New Folder (3)']
+const newFolder = newFolderName(folders)
+console.log(newFolder) // Output: 'New Folder (4)'

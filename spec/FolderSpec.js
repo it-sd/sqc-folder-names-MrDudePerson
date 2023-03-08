5f7ac8d90661e397ad4567160205cced93b5baf3
Joseph Green <dudeiera@gmail.com>
2023-03-07 19:56:48 -0600

    Commiting code that succesfully passes all jasmine tests with 100 percent npm run coverage

--- spec/FolderSpec.js
+++ spec/FolderSpec.js
@@ -1,23 +1,24 @@
 const { newFolderName } = require('../server.js')
 
+// Test Case 1
 describe('folder names', function () {
   it('should generate "New Folder" with an empty array', function () {
     const result = newFolderName([])
     expect(result).toBe('New Folder')
   })
-
-  it('should generate "New Folder" if that name does\'t exist', function () {
+  // Test Case 2
+  it('should generate "New Folder" if that name dont exist', function () {
     expect(newFolderName(['a folder', 'new folder'])).toBe('New Folder')
 
     const folders = ['New Folder (1)', 'New Folder (2)', 'New Folder (3)']
     expect(newFolderName(folders)).toBe('New Folder')
   })
-
+  // Test Case 3
   it('should generate "New Folder (2)" if "New Folder" exists', function () {
     const folders = ['Documents', 'New Folder (5)', 'New Folder']
     expect(newFolderName(folders)).toBe('New Folder (2)')
   })
-
+  // Test Case 4
   it('should generate the next available "New Folder (n + 1)"', function () {
     const folders0 = ['New Folder (2)', 'New Folder']
     expect(newFolderName(folders0)).toBe('New Folder (3)')
